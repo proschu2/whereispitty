15aadecefaef8ed374e49809e27fa23ceaa3fa02
Sanzio Monti <github@sanziomonti.xyz>
2022-12-29 23:57:38 +0100

    add flags instead of markers

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import './App.css';
+import "./App.css";
 
-import React from 'react';
+import React from "react";
 
-import PitMap from './PitMap';
+import PitMap from "./PitMap";
 
 function App() {
   return (
--- src/PitMap.tsx
+++ src/PitMap.tsx
@@ -1,42 +1,49 @@
-import { Map, Marker } from 'pigeon-maps';
-import React, { useState } from 'react';
+import { Map, Marker, Overlay } from "pigeon-maps";
+import React, { useState } from "react";
 
-import data from './data/locations.json';
-import useWindowDimensions from './hooks/useWindowDimensions';
+import data from "./data/locations.json";
+import useWindowDimensions from "./hooks/useWindowDimensions";
+import { CircleFlag } from "react-circle-flags";
 
 // import { maptiler } from "pigeon-maps/providers";
 
 // const maptilerProvider = maptiler(process.env.REACT_APP_MAPTILER_API_KEY, "basic-v2-light");
-
+const initialCenter: [number, number] = [48.71466750292578, 10.961941600930126];
 const PitMap = () => {
   const { height, width } = useWindowDimensions();
-  const [center, setCenter] = useState([47.39033, 8.48588]);
-  const [zoom, setZoom] = useState(6);
-
+  const [center, setCenter] = useState<[number, number]>(initialCenter);
+  const [zoom, setZoom] = useState<number>(6);
+  const resetMap = () => {
+    setCenter(initialCenter);
+    setZoom(6);
+  };
   return (
     <Map
       // provider={maptilerProvider}
       height={height}
       width={width}
-      center={[center[0], center[1]]}
+      center={center}
       zoom={zoom}
       minZoom={5}
-      onClick={() => setZoom(6)}
+      onClick={resetMap}
       onBoundsChanged={({ center, zoom }) => {
         setCenter(center);
         setZoom(zoom);
       }}
     >
-      {data.map((loc) => (
-        <Marker
-          width={50}
-          anchor={[loc.lat, loc.lon]}
-          onClick={() => {
-            setCenter([loc.lat, loc.lon]);
-            setZoom(8);
-          }}
-        />
-      ))}
+      {data &&
+        data.map((loc) => (
+          <Overlay anchor={[loc.lat, loc.lon]} offset={[0, 0]}>
+            <CircleFlag
+              countryCode={loc.country.toLowerCase()}
+              height="20"
+              onClick={() => {
+                setCenter([loc.lat, loc.lon]);
+                setZoom(8);
+              }}
+            />
+          </Overlay>
+        ))}
     </Map>
   );
 };
