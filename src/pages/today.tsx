b852aab6b8596c884accf82a445da8a7c7f76d8f
Sanzio Monti <github@sanziomonti.xyz>
2022-12-31 16:26:41 +0100

    today - replace day with modal

--- src/pages/today.tsx
+++ src/pages/today.tsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "../data/locations.json";
 import Day from "../components/day";
 import { location } from "../data/location";
+import Detail from "../components/detail";
+
 const Today = () => {
   const today = new Date().toLocaleDateString("en-GB");
   const potentialLocations: location[] = data.filter(
     (loc) => loc.date === "*" || loc.date === today
   );
-  return <Day loc={potentialLocations[potentialLocations.length - 1]} />;
+  const [open, setOpen] = useState<boolean>(true);
+  const closeModal = () => {
+    setOpen(false);
+  };
+  return (
+    <Detail
+      closeModal={closeModal}
+      open={open}
+      props={potentialLocations[potentialLocations.length - 1]}
+    />
+  );
 };
 
 export default Today;
