b95c5f4f8750955f290019ef1bfa32b0f7344732
Sanzio Monti <github@sanziomonti.xyz>
2023-01-01 16:58:02 +0100

    implement getDay

--- src/components/day.tsx
+++ src/components/day.tsx
@@ -7,23 +7,10 @@ import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Unstable_Grid2";
 import Divider from "@mui/material/Divider";
 import Card from "@mui/material/Card";
-import Picture from "./picture";
 import { location } from "../data/location";
-import Concert from "./concert";
-import Casina from "./casina";
 import { useParams } from "react-router-dom";
-import data from "../data/locations.json";
 import Info from "./info";
-
-const getDay = (id?: string): location => {
-  if (typeof id === "undefined") {
-    return data.filter((l) => l.date === "*")[0];
-  }
-  const potentialLocations = data.filter(
-    (l) => l.id === parseInt(id) || l.date === "*"
-  );
-  return potentialLocations[potentialLocations.length - 1];
-};
+import { getDay } from "../utils/concert";
 
 const Day: FC<{ loc?: location }> = ({ loc }) => {
   const { id } = useParams();
--- src/utils/concert.ts
+++ src/utils/concert.ts
@@ -1,6 +1,24 @@
+import locations from "../data/locations.json";
+import { location } from "../data/location";
+import data from "../data/locations.json";
 
 const homePics = ["coffee.webp", "couch.webp"];
-const concertPics = ["pit_tour1.webp", "pit_tour2.webp", "pit_tour3.webp"];
+const concertPics = [
+  "pit_tour1.webp",
+  "pit_tour2.webp",
+  "pit_tour3.webp",
+  "pit_tour4.webp",
+];
+
+export const getDay = (id?: string): location => {
+  if (typeof id === "undefined") {
+    return data.filter((l) => l.date === "*")[0];
+  }
+  const potentialLocations = data.filter(
+    (l) => l.id === parseInt(id) || l.date === "*"
+  );
+  return potentialLocations[potentialLocations.length - 1];
+};
 
 const randomGen = (items: Array<string>): number => {
   return Math.floor(Math.random() * items.length);
