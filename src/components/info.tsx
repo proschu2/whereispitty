4bae00f6f647315a54b12666df93666351e7b03d
Sanzio Monti <github@sanziomonti.xyz>
2023-01-02 23:02:47 +0100

    change cursor to red into modal

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -6,14 +6,14 @@ import Casina from "./casina";
 
 const Info: FC<{ loc: location; concert?: string }> = ({ loc, concert }) => {
   return (
-    <>
+    <div className="redrose">
       <Picture
         className="whiterose"
         type={loc ? loc.type : "HOME"}
         concert={concert}
       />
       {loc.type === "CONCERT" ? <Concert {...loc} /> : <Casina />}
-    </>
+    </div>
   );
 };
 
